6b5096aa6e3628af9398c1547a7c111a1d81c806
Samay Nathani <46657835+samayNathani@users.noreply.github.com>
2020-07-31 22:25:29 -0700

    init

--- hellotweepy.py
+++ hellotweepy.py
@@ -7,7 +7,7 @@ auth = tweepy.OAuthHandler(tokens.api_key, tokens.api_secret_key)
 auth.set_access_token(tokens.access_token, tokens.secret_access_token)
 
 api = tweepy.API(auth)
-search_results = api.search(q="BLM protest filter:images", count=100)
+search_results = api.search(q="#protest filter:images", count=100)
 for tweet in search_results:
         media = tweet.entities.get('media', [])
         if len(media) != 0:
