41081f317150c404b4b4c2fa17555127a16352b3
Samay Nathani <46657835+samayNathani@users.noreply.github.com>
2020-07-31 22:02:38 -0700

    working facial recognition feature on BLM protes images

---
+++ detect_face.py
@@ -0,0 +1,21 @@
+import cv2
+from skimage import io
+
+def detect_face(img_url):
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    # Read the input image
+    img = io.imread(img_url)
+    # Convert into grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    #     # Draw rectangle around the faces
+    if len(faces) == 0:
+        print("NO FACE FOUND")
+    else:
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    # Display the output
+        cv2.imshow('img', cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+        cv2.waitKey()
---
+++ hellotweepy.py
@@ -0,0 +1,15 @@
+import tweepy
+import tokens
+from detect_face import detect_face
+
+
+auth = tweepy.OAuthHandler(tokens.api_key, tokens.api_secret_key)
+auth.set_access_token(tokens.access_token, tokens.secret_access_token)
+
+api = tweepy.API(auth)
+search_results = api.search(q="BLM protest filter:images", count=100)
+for tweet in search_results:
+        media = tweet.entities.get('media', [])
+        if len(media) != 0:
+                detect_face(media[0]['media_url'])
+        #print(media)
